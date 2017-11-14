4badf95205c419a3698f4da245dc35d706267a97
alex.mabry <alex.mabry@synapse-wireless.com>
2017-11-14 14:23:09 -0600

    Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const webpack = require('webpack');
 
 let config = {
-    entry: './src/index.js',
-    output: {
-        filename: 'public/js/bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'public/js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.ttf([?]?.*)$/,
+        loader: 'file-loader'
+      }]
+  }
 };
 
 module.exports = config;

78c3fb4c163ec6925ea4935d2b4f75881d7ed3a2
alex.mabry <alex.mabry@synapse-wireless.com>
2017-11-14 15:23:30 -0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 require('@atlassian/aui/dist/aui/css/aui.min.css');
 require('./addon.css');
+
+const $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
 require('underscore/underscore-min.js');
 require('@atlassian/aui/dist/aui/js/aui.min.js');
 require('@atlassian/aui/dist/aui/js/aui-datepicker.min.js');
